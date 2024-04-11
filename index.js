9827a1d059ac1771acc124c2ef8739be9ff85b10
buhleqampi <buhle3857@gmail.com>
2024-04-11 02:58:43 +0200

    new project

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+const messages = JSON.parse(localStorage.getItem('messages')) || [];
 const searchInput = document.querySelector('.searchInput');
 const messageInput = document.querySelector('#messageInput');
-const deleteButton = document.querySelector('#button')
+const sendButton = document.querySelector('#button');
+const newMessage = document.querySelector('#single-mssg');
+
+sendButton.addEventListener('click',messageSend);
+
+function messageSend() {
+    const messageValue = messageInput.value.trim();
+   
+if (messageValue !== "") {
+    const timestamp = new Date().toLocaleString();
+    const message = {
+        messageValue,
+        timestamp
+    };
+    messages.push(message);
+    saveTasksToLocalStorage();
+    renderMessages();
+    messageInput.value = '';
+    
+} else {
+    alert("Please enter a message!");
+}
+}
+
+function renderMessages() {
+    
+    newMessage.innerHTML = '';
+    messages.slice().reverse().forEach((message, index) => {
+        const li = document.createElement('li');
+        li.className = 'list-group-message';
+        li.innerHTML = ` ${message.messageValue}<br>
+                        ${message.timestamp}<br>
+                        <i onclick="delMessage(${tasks.length - 1 - index})" class="fa-solid fa-trash"></i>`;
+        newMessage.appendChild(li);
+});
+}
+    
